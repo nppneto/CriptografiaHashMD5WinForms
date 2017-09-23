b90e9aaa77d4239fe1657dcc2341825882f55eb8
Nelson Neto <nelsonp2n@gmail.com>
2017-09-23 11:51:02 -0300

    Estudo WinForms

--- CriptografiaMD5/Form1.cs
+++ CriptografiaMD5/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace CriptografiaMD5
 {
     public partial class Form1 : Form
     {
+        Password p = new Password();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            p.Senha = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder senhaCript = new StringBuilder();
+            MD5 md5 = MD5.Create();
+
+            byte[] entrada = Encoding.ASCII.GetBytes(p.Senha);
+            byte[] hash = md5.ComputeHash(entrada);
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                senhaCript.Append(hash[i].ToString("X2"));
+            }
+
+            MessageBox.Show("Senha criptografada: " + senhaCript, "Resposta ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
         }
     }
 }
---
+++ CriptografiaMD5/Password.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriptografiaMD5
+{
+    class Password
+    {
+        public string Senha { get; set; }
+    }
+}
